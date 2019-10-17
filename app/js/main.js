31e6427ef4cfa3c9f640d8faf8e492a582953671
Denis <kdb2015@mail.ru>
2019-10-17 17:20:12 +0300

    portfolio

--- app/js/main.js
+++ app/js/main.js
@@ -17,4 +17,39 @@ $(document).ready(function() {
   $("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
       highlightSelector:"nav a"
   });
+
+//Owl carousel  slider
+$('.portfolio').owlCarousel ({
+    loop: false,
+    margin: 10,
+    dots: false,
+  loop: true,
+    margin: 10,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1,
+            dotsEach: 5000,
+            //nav: true,
+        },
+
+        992: {
+            items: 1,
+            nav: false,
+            //autoplay: 1000,
+            //autoplaySpeed: 5000,
+            //dotsEach: 5000,
+            //autoplayTimeout: 12000
+        },
+
+        1200: {
+            items: 2,
+            nav: true,
+            //autoplay: 1000,
+            //autoplaySpeed: 4000,
+            //dotsEach: 5000,
+            //autoplayTimeout: 8000
+        },
+    }
+});
 });
